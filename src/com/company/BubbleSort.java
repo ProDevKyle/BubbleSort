6f949eea9ad82d2a8c6583d3ab4d653560e6e236
Kyle <ctam9591@bths.edu>
2018-11-14 20:17:30 -0500

    update

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -28,6 +28,17 @@ public class BubbleSort
             }
         }
     }
+    public static void selectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[i] > arr[j])
+                    swap(arr, i, j);
+            }
+        }
+    }
     public static void string(int[] arr)
     {
         String str = "";
